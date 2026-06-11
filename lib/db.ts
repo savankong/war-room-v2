e8f0c9e0b120c24faafe09d9c01ee2adb3f2c946
Savan Kong <savankong@Savans-MacBook-Pro.local>
2026-06-11 12:26:48 -0700

    Switch db.ts from @netlify/database to postgres pkg using DATABASE_URL

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,10 @@
-import { getDatabase } from '@netlify/database';
+import postgres from 'postgres';
 
-export function getDb() {
-  return getDatabase().sql;
+if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL not set');
+
+const sql = postgres(process.env.DATABASE_URL, { ssl: 'require' });
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function getDb(): any {
+  return sql;
 }
